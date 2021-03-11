fdda92d8c15582b4a5934afaf9ec1b52d882c013
xaboandras <48335971+xaboandras@users.noreply.github.com>
2021-03-11 10:11:33 +0100

    1. feladat
    a kijelölt mező keretszíne

--- LAB10_AttaxxPlus/AttaxxPlus/View/IsSelected2BrushConverter.cs
+++ LAB10_AttaxxPlus/AttaxxPlus/View/IsSelected2BrushConverter.cs
@@ -8,12 +8,12 @@ namespace AttaxxPlus.View
     public class IsSelected2BrushConverter : IValueConverter
     {
         // EVIP: reusing brushes, named constants
-        readonly private static SolidColorBrush yellow = new SolidColorBrush(Colors.Yellow);
+        readonly private static SolidColorBrush crimson = new SolidColorBrush(Colors.Crimson);
         readonly private static SolidColorBrush gray = new SolidColorBrush(Colors.Gray);
 
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            return ((bool)value) ? yellow : gray;
+            return ((bool)value) ? crimson : gray;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
